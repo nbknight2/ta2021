de651f7734800f90f2e41ae81be6770345d4078a
Nick Knight <nicholas.knight@levvel.io>
2021-06-29 11:40:14 -0400

    Completed data module and updated main.tf.bak

--- kplabs/section4/data_sources/basic-config.tf
+++ kplabs/section4/data_sources/basic-config.tf
@@ -9,14 +9,32 @@
 # ///////////////////////
 # Provider
 # ///////////////////////
+provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region = var.region
+}
 
 # ///////////////////////
 # Resource
 # ///////////////////////
+resource "aws_instance" "instance-1" {
+  ami = data.aws_ami.app_ami.id
+  instance_type = "t2.micro"
+}
 
 # ///////////////////////
 # Data
 # ///////////////////////
+data "aws_ami" "app_ami" {
+  most_recent = true
+  owners = ["amazon"]
+
+  filter {
+    name = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+}
 
 # ///////////////////////
 # Output
